3ecdf34217020d88803b880480f410e0b2f7a285
tusha-p <45887071+tusha-p@users.noreply.github.com>
2025-08-23 22:37:41 -0700

    Create BaseTest.java

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,40 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setUp(String browser) {
+        // Initialize the WebDriver based on the parameter from testng.xml
+        if (browser.equalsIgnoreCase("chrome")) {
+            // You can use WebDriverManager here in a real project to avoid manual driver setup
+            // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            // System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
+            driver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        // A common starting point for our mock app. We will create this later.
+        driver.get("http://localhost:8080"); 
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
