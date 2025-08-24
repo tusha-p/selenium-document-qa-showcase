4a1fb0805628fb3856bcac4b35c6ec7712c1c80f
tusha-p <45887071+tusha-p@users.noreply.github.com>
2025-08-24 06:51:53 -0700

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,40 +1,22 @@
-package base;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import java.io.File;
+import java.util.UUID;
 
 public class BaseTest {
-
     protected WebDriver driver;
-
-    @BeforeMethod(alwaysRun = true)
-    @Parameters("browser")
-    public void initializeDriver(String browser) {
-        // Initialize the WebDriver based on the parameter from testng.xml
-        if (browser.equalsIgnoreCase("chrome")) {
-            // You can use WebDriverManager here in a real project to avoid manual driver setup
-            // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-            driver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            // System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
-            driver = new FirefoxDriver();
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-
-        driver.manage().window().maximize();
-        // A common starting point for our mock app. We will create this later.
-        driver.get("http://localhost:8080"); 
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDownDriver() {
-        if (driver != null) {
-            driver.quit();
-        }
+    
+    protected void initializeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        
+        // Add unique user data directory
+        String uniqueUserDataDir = "/tmp/chrome-profile-" + UUID.randomUUID().toString();
+        options.addArguments("--user-data-dir=" + uniqueUserDataDir);
+        
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
